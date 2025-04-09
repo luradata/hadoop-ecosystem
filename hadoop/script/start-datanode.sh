be7be1c9d48c58af57d5a6574b1a39075727bb06
Hai Pham Ngoc <ngochai285nd@gmail.com>
2025-04-09 14:46:55 +0700

    feat: update hdfs site

--- hadoop/script/start-datanode.sh
+++ hadoop/script/start-datanode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf /hadoop/dfs/datanode/*
-chown -R nobody:nobody /hadoop/dfs/datanode
-chmod 755 /hadoop/dfs/datanode
+# rm -rf /hadoop/dfs/datanode/*
+# chown -R nobody:nobody /hadoop/dfs/datanode
+# chmod 755 /hadoop/dfs/datanode
 hdfs datanode
